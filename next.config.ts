92b425a4b717e455643a4db9bab365fe99b6e543
aifirst8601 <aifirst8601@users.noreply.github.com>
2026-03-21 17:04:50 +0800

    fix: cannot use output export with dynamic API route, revert to standalone

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* Config options for Cloudflare Pages */
-  output: "export",
+  output: "standalone",
   eslint: {
     // 忽略构建时的 ESLint 检查警告/错误，不影响运行
     ignoreDuringBuilds: true,
   },
   basePath: "/bgremover",
-  distDir: "out",
 };
 
 export default nextConfig;
